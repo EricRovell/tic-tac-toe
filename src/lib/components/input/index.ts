5857e3a3b423ff7dd195e4fdb33069e374873120
Eric Rovell <ericrovell@gmail.com>
2021-04-28 11:54:08 +0300

    add: components/input index

---
+++ src/lib/components/input/index.ts
@@ -0,0 +1,6 @@
+export { default as Button } from "./Button.svelte";
+export { default as InputNumber } from "./InputNumber.svelte";
+export { default as RadioGroup } from "./RadioGroup.svelte";
+export { default as RadioButton } from "./RadioButton.svelte";
+export { default as Select } from "./Select.svelte";
+export { default as SwitcherTheme } from "./SwitcherTheme.svelte";
