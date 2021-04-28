349a56db33df43a1090435b5204d070e0581523b
Eric Rovell <ericrovell@gmail.com>
2021-04-28 11:46:11 +0300

    add: Mark component

---
+++ src/lib/components/board/Mark.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import DefaultSkin from "./skins/Default.svelte";
+  import type { BoardMark } from "$types";  
+  
+  export let mark: BoardMark = null;
+</script>
+
+<DefaultSkin value={mark} />
