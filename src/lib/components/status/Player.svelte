be49f42c169010a0e5fab9e93faf6c767540fc2e
Eric Rovell <ericrovell@gmail.com>
2021-04-30 02:51:55 +0300

    add: Player Mark component

---
+++ src/lib/components/status/Player.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { inGame, currentPlayer } from "$stores/status";
+  import { Icon } from "$components/svg";
+  import { classic } from "$components/board/skins/svg-default";
+  
+  export let mark = 1;
+  
+  $: active = $inGame && mark === $currentPlayer;
+</script>
+
+<Icon path={classic[mark]} color="primary" glow={active} />
