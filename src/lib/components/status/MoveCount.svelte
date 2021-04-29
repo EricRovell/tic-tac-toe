5373859def28002afc8c71c70a318713c4d93d69
Eric Rovell <ericrovell@gmail.com>
2021-04-30 02:52:05 +0300

    add: MoveCount component

---
+++ src/lib/components/status/MoveCount.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { moveCount } from "$stores/board";
+  import { Icon, iconCount } from "$components/svg";
+</script>
+
+<output>
+  <Icon path={iconCount} />
+  <span>{$moveCount}</span>
+</output>
+
+<style>
+  output {
+    display: flex;
+    place-items: center;
+    gap: 0.25em;
+    
+    font-size: var(--font-size-4);
+  }
+</style>
