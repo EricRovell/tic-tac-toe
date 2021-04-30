d392fdc7bc49f150ff6d69d58ab0931a689594c8
Eric Rovell <ericrovell@gmail.com>
2021-04-30 11:58:36 +0300

    add: Game View

---
+++ src/lib/views/Game.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { Board, Message } from "$components/board";
+  import { Status } from "$components/status";
+  import { inGame } from "$stores/status";
+</script>
+
+<main>
+  <Status />
+  <Board active={$inGame} />
+  <Message />
+</main>
+
+<style>
+	main {
+    display: grid;
+    grid-template: min-content 1fr auto / 1fr;
+    gap: 1.5em;
+    width: min(1280px, 100%);
+    min-height: 100vh;
+    place-items: start center;
+	}
+  
+  @media (max-width: 540px) {
+    main {
+      padding: 1em;
+    }
+  }
+</style>
