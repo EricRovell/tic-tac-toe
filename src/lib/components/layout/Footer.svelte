1247cfe6d3e7aa37b0918b9eb420a30ac43cab7c
Eric Rovell <ericrovell@gmail.com>
2021-04-30 11:50:40 +0300

    add: Footer component

---
+++ src/lib/components/layout/Footer.svelte
@@ -0,0 +1,22 @@
+<footer>
+  <div class="wrapper">
+    <span>Tic Tac Toe ver. 0.1</span>
+  </div>
+</footer>
+
+<style>
+  footer {
+    display: grid;
+    place-items: center;
+    padding: 0.25em 1em;
+    width: 100%;
+    background-color: #161b22;
+  }
+  
+  .wrapper {
+    display: grid;
+    place-items: start;
+    width: min(1280px, 100%);
+    padding: 0.75em 0;
+  }
+</style>
