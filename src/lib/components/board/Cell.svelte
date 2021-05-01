316840f2759c5b0b0fd981c039856cdba516a97c
Eric Rovell <ericrovell@gmail.com>
2021-05-02 01:43:46 +0300

    upd: cell theme styles

--- src/lib/components/board/Cell.svelte
+++ src/lib/components/board/Cell.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { history } from "$stores/board";
   import { currentPlayer } from "$stores/status";
+  import { dark } from "$stores/theme";
   import type { BoardMark } from "$types";
   
   import Mark from "./Mark.svelte";
@@ -16,23 +17,39 @@
   }
 </script>
 
-<button on:click={handleMark} disabled={disabled || !!value}>
-  <Mark {value} />
+<button
+  class="cell"
+  class:dark={$dark}
+  on:click={handleMark}
+  disabled={disabled || !!value}>
+    <Mark {value} />
 </button>
 
 <style>
-  button {
+  .cell.dark {
+    --cell-bg: var(--color-surface-400);
+    --cell-mark-color: lightcoral;
+  }
+  
+  .cell:not(.dark) {
+    --cell-bg: #E15730;
+    --cell-mark-color: var(--color-surface-300);
+  }
+  
+  .cell {
     aspect-ratio: 1;
     cursor: pointer;
-    background-color: #21262d;
+    background-color: var(--cell-bg);
+    color: var(--cell-mark-color);
     width: 75px;
     height: 75px;
     padding: 0.5em;
   }
   
-  button:disabled {
+  .cell:disabled {
     opacity: unset;
-    border-color: var(--color-danger);
+    outline: 3px solid var(--color-gray-500);
+    outline-offset: -3px;
     cursor: default;
   }
 </style>
