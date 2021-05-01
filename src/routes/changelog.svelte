581d5132c7879fa0d39361b000cf4bd33bbdacc9
Eric Rovell <ericrovell@gmail.com>
2021-05-01 23:34:57 +0300

    add: changelog route

---
+++ src/routes/changelog.svelte
@@ -0,0 +1,42 @@
+<article>
+  <header>
+    <h1>Changelog</h1>
+  </header>
+  <section>
+    <h2>Version 0.1</h2>
+    <ol>
+      <li>First public release</li>
+    </ol>
+  </section>
+</article>
+
+<style>
+  article {
+    display: grid;
+    gap: 0.5em;
+    
+    padding: 1.5em;
+    width: min(80ch, 100%);
+    
+    border: 1px solid rgb(255 255 255 / 0.25);
+    border-radius: var(--radius-2);
+  }
+  
+  header {
+    font-size: var(--font-size-5);
+  }
+  
+  section h2 {
+    font-size: var(--font-size-4);
+  }
+  
+  section {
+    display: grid;
+    gap: 0.5em;
+  }
+  
+  ol {
+    list-style: circle;
+    list-style-position: inside;
+  }
+</style>
